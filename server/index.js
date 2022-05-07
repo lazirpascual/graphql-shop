9268ebfadf4346c2b68fb5c62cc2f0bc18c18fb8
Lazir Pascual <lazirpascual@gmail.com>
2022-05-07 04:14:10 -0400

    switch host and server back to original

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ Shopify.Context.initialize({
   API_KEY: process.env.SHOPIFY_API_KEY,
   API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
   SCOPES: process.env.SCOPES.split(","),
-  HOST_NAME: "https://graphql-shop.herokuapp.com",
+  HOST_NAME: process.env.HOST.replace(/https:\/\//, ""),
   API_VERSION: ApiVersion.April22,
   IS_EMBEDDED_APP: true,
   // This should be replaced with your preferred storage strategy
